c9b26d9eec7dcd3d35db02ff580dfd0ead995147
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-02 22:24:46 -0300

    feat: product route

--- src/main/java/com/themyntt/store/controller/Product.java
+++ src/main/java/com/themyntt/store/controller/Product.java
@@ -1,4 +1,10 @@
 package com.themyntt.store.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/product/")
 public class Product {
+
 }
