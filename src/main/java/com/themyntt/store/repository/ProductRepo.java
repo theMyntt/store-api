628742b40c031e5bf6f00c7697052e9daa5102c1
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-03 20:45:12 -0300

    feat: insert query on repository

--- src/main/java/com/themyntt/store/repository/ProductRepo.java
+++ src/main/java/com/themyntt/store/repository/ProductRepo.java
@@ -1,6 +1,8 @@
 package com.themyntt.store.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.themyntt.store.entity.ProductEntity;
@@ -10,5 +12,10 @@ import java.util.List;
 @Repository
 public interface ProductRepo extends JpaRepository<ProductEntity, String> {
     @Query("SELECT p FROM ProductEntity p")
-    List<ProductEntity> findAll();
+    List<ProductEntity> getAllProducts();
+
+    @Modifying
+    @Transactional
+    @Query("INSERT INTO ProductEntity(id, name, desc, company, value) VALUES (:id, :name, :desc, :company, :value)")
+    ProductEntity setProduct(String id, String name, String desc, String company, double value);
 }
