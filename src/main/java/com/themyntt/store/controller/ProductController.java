00e4c91ae67c382a7be34e4177527d3a916b1abc
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-04-03 21:05:02 -0300

    feat: set product route

--- src/main/java/com/themyntt/store/controller/ProductController.java
+++ src/main/java/com/themyntt/store/controller/ProductController.java
@@ -2,21 +2,39 @@ package com.themyntt.store.controller;
 
 import com.themyntt.store.entity.ProductEntity;
 import com.themyntt.store.repository.ProductRepo;
+import com.themyntt.store.util.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/product/")
 public class ProductController {
     @Autowired
     private ProductRepo productRepo;
+    private Utils utils = new Utils();
 
-    @GetMapping("/get/")
+    @GetMapping("/get/all/")
     public List<ProductEntity> getProducts() {
         return productRepo.getAllProducts();
+    }
+
+    @PostMapping("/set/")
+    public String setProduct(@RequestBody ProductEntity productInfo) {
+        String id = utils.createId();
+        String name = productInfo.name;
+        String desc = productInfo.desc;
+        String company = productInfo.company;
+        double value = productInfo.value;
+
+        try {
+            productRepo.setProduct(id, name, desc, company, value);
+            return "OK";
+        } catch (Exception e) {
+            return "Erro ao inserir";
+        }
+
     }
 }
